35e418127b21481d0fb6efea992fbca3132d7a10
JSeoyeong <irenejsy@naver.com>
2024-04-04 11:05:43 +0900

    first commit

---
+++ project_0327.c
@@ -0,0 +1,70 @@
+/*
+#include <stdio.h>
+int main(void)
+{
+
+    # 숫자 입력 받기
+    int a;
+    printf("숫자 하나를 입력하세요: ");
+    scanf("%d", &a);
+    
+    printf("입력한 숫자는 %d입니다. \n", a);
+    
+    # ASCII
+    printf("%d \n", 'A');
+    printf("%c \n", 'A');
+    
+    printf("%d \n", 3/2);
+    
+    # 형 변환
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%f \n", (double)a/(double)b);
+    
+    # If
+    int a = 10, b = 10;
+    if (a == b) {
+        printf("a와 b는 같다 \n");
+    }
+    else {
+        printf("a와 b는 다르다 \n");
+    }
+    
+    int a = 10, b = 10;
+    if (a > b) {
+        printf("a는 b보다 크다 \n");
+    }
+    else if (a == b) {
+        printf("a와 b는 같다 \n");
+    }
+    else {
+        printf("a와 b는 다르다 \n");
+    }
+    
+    # While
+    int i = 0;
+    while (++i<10) {
+        printf("%d \n", i);
+    }
+    
+    int i = 0;
+    while (i++<10) {
+        printf("%d \n", i);
+    }
+    
+    
+    
+    # For
+    for (int i = 0; i<10; i++) {
+        printf("%d \n", i);
+    }
+    
+    # 변수를 밖에서 지정하는 것도 가능하긴 함.
+    int i = 0;
+    for (; i<10; i++) {
+        printf("%d \n", i);
+    }
+    
+    return 0;
+}
+*/
---
+++ project_0403.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+/*
+int add(int x, int y) {
+    return x+y;
+}
+
+void menu(void) {
+    printf("1. Add \n");
+}
+int main(void) {
+    int a, b;
+    menu();
+    a = 10, b = 20;
+    printf("Sum of %d and %d is %d \n", a, b, add(a,b));
+    return 0;
+}
+
+void swap(int, int);
+
+int main(void){
+    int a = 10, b = 20;
+    swap(a, b);
+    printf("%d %d \n", a, b);
+    
+    return 0;
+}
+
+void swap(int x, int y){    //call by value, call by reference
+    int tmp;
+    tmp = x;
+    x = y;
+    y = tmp;
+}
+
+void recursive(int);
+
+int main(void){
+    recursive(10);
+    return 0;
+}
+void recursive(int n){
+    printf("recursive call! \n");
+    if (n==1) return;
+    recursive(n-1);
+}
+
+
+#include<stdlib.h>
+
+int main(void){
+    int i;
+    i = rand();
+    printf("%d \n", i);
+    return 0;
+}
+
+#include<stdlib.h>
+#include<time.h>
+
+int main(void){
+    srand((unsigned)time(NULL));
+    int i;
+    i = rand()%45+1;
+    printf("%d \n", i);
+    return 0;
+}
+ */
